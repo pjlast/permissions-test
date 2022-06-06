b69a09f22119b94e2ed21ee21864d7bea9a3e370
Petri-Johan Last <petri.last@sourcegraph.com>
2022-06-06 13:15:32 +0200

    Show how permissions are shared between objects

--- main.go
+++ main.go
@@ -25,13 +25,19 @@ func setupDB() (func(), error) {
 		return nil, err
 	}
 
-	m.Steps(1)
+	m.Up()
 
-	return func() { m.Steps(-1) }, nil
+	return func() { m.Down() }, nil
 }
 
 func createRelation(db *sql.DB, objectNamespace string, objectID int, relation, subjectNamespace string, subjectID int, subjectSetRelation string) error {
 	_, err := db.Exec("INSERT INTO permissions(object_namespace, object_id, relation, subject_namespace, subject_id, subject_set_relation) VALUES($1, $2, $3, $4, $5, $6);", objectNamespace, objectID, relation, subjectNamespace, subjectID, subjectSetRelation)
+
+	return err
+}
+
+func deleteRelation(db *sql.DB, objectNamespace string, objectID int, relation, subjectNamespace string, subjectID int, subjectSetRelation string) error {
+	_, err := db.Exec("DELETE FROM permissions WHERE object_namespace = $1 AND object_id = $2 AND relation = $3 AND subject_namespace = $4 AND subject_id = $5 AND subject_set_relation = $6;", objectNamespace, objectID, relation, subjectNamespace, subjectID, subjectSetRelation)
 
 	return err
 }
@@ -51,6 +57,7 @@ with recursive all_users as (
 	where 
 		object_namespace = $1
 		and object_id = $2
+		and relation = $4
 	union 
 		select 
 			p.object_namespace,
@@ -62,7 +69,7 @@ with recursive all_users as (
 		from
 			permissions p
 		inner join all_users au on au.subject_namespace = p.object_namespace and au.subject_id = p.object_id and au.subject_set_relation=p.relation
-) select subject_id from all_users where subject_namespace = $3 AND relation = $4;
+) select subject_id from all_users where subject_namespace = $3;
 `
 
 	rows, err := db.Query(sqlQuery, objectNamespace, objectID, subjectNamespace, relation)
@@ -102,12 +109,132 @@ func main() {
 	}
 	defer db.Close()
 
+	fmt.Println("First we show that, without any permission entries, user 1 cannot access notebook 1")
+
+	canRead, err := checkRelation(db, "notebooks", 1, "read", "users", 1)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Can user 1 read notebook 1?: ", canRead)
+
+	fmt.Println("Now we create an entry giving user 1 direct read access to notebook 1, and check again.")
+
 	err = createRelation(db, "notebooks", 1, "read", "users", 1, "")
 	if err != nil {
 		panic(err)
 	}
 
-	canRead, err := checkRelation(db, "notebooks", 1, "read", "users", 1)
+	canRead, err = checkRelation(db, "notebooks", 1, "read", "users", 1)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Can user 1 read notebook 1?: ", canRead)
+
+	fmt.Println("-------")
+
+	fmt.Println("User 1 should still be unable to write to notebook 1.")
+
+	canWrite, err := checkRelation(db, "notebooks", 1, "write", "users", 1)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Can user 1 write to notebook 1?: ", canWrite)
+
+	fmt.Println("We are now going to give user 1 indirect write access to notebook 1. We will add user 1 to a group, and then give members of that group write access to notebook 1")
+
+	err = createRelation(db, "groups", 1, "member", "users", 1, "")
+	err = createRelation(db, "notebooks", 1, "write", "groups", 1, "member")
+
+	fmt.Println("Now, with user 1 part of group 1 which has write access to notebooks 1, we check again:")
+
+	canWrite, err = checkRelation(db, "notebooks", 1, "write", "users", 1)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Can user 1 write to notebook 1?: ", canWrite)
+
+	fmt.Println("---------")
+
+	fmt.Println("Next we want to show that we can link a code insight's permissions to the notebook, and by extension give user 1 access to the code insight.")
+
+	fmt.Println("First we show that, without any permission entries, user 1 cannot access the code insight")
+
+	canRead, err = checkRelation(db, "codeinsights", 1, "read", "users", 1)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Can user 1 read codeinsight 1?: ", canRead)
+
+	fmt.Println("Now we give readers of notebook 1 read access to code insight 1, and check again")
+
+	err = createRelation(db, "codeinsights", 1, "read", "notebooks", 1, "read")
+
+	canRead, err = checkRelation(db, "codeinsights", 1, "read", "users", 1)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Can user 1 read codeinsight 1?: ", canRead)
+
+	fmt.Println("------")
+
+	fmt.Println("Further more, we show that we can give writers of notebook 1 write access to code insight 1.")
+	fmt.Println("In this case, user 1 can now write to code insight 1, because he is a member of group 1 which has write access to notebook 1, which now has write access to code insight 1")
+
+	err = createRelation(db, "codeinsights", 1, "write", "notebooks", 1, "write")
+
+	canWrite, err = checkRelation(db, "codeinsights", 1, "write", "users", 1)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Can user 1 write to codeinsight 1?: ", canWrite)
+
+	fmt.Println("------")
+	fmt.Println("Finally, we remove user 1 from group 1, which should cause them to lose write access to notebook 1, but they will still be able to read it and code insight 1")
+
+	err = deleteRelation(db, "groups", 1, "member", "users", 1, "")
+	if err != nil {
+		panic(err)
+	}
+
+	canWrite, err = checkRelation(db, "codeinsights", 1, "write", "users", 1)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Can user 1 write to codeinsight 1?: ", canWrite)
+
+	canWrite, err = checkRelation(db, "notebooks", 1, "write", "users", 1)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Can user 1 write to notebook 1?: ", canWrite)
+
+	canRead, err = checkRelation(db, "codeinsights", 1, "read", "users", 1)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Can user 1 read codeinsight 1?: ", canRead)
+
+	canRead, err = checkRelation(db, "notebooks", 1, "read", "users", 1)
 
 	if err != nil {
 		panic(err)
